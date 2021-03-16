80393ec874ea28ddb6fb2aa22b8fd4431a1b2725
Lari Löfman <larwazor@gmail.com>
2021-03-16 12:06:45 +0200

    add health check

---
+++ server/controllers/healthController.js
@@ -0,0 +1,7 @@
+const checkHealth = async (req, res) => {
+  res.send('ok');
+};
+
+module.exports = {
+  checkHealth,
+};
--- server/util/routes.js
+++ server/util/routes.js
@@ -1,5 +1,6 @@
 const Router = require('express');
 const notes = require('@controllers/notesController');
+const healthCheck = require('@controllers/healthController');
 
 const router = Router();
 
@@ -7,5 +8,6 @@ router.get('/notes', notes.getAll);
 router.post('/notes', notes.create);
 router.delete('/notes', notes.destroyAll);
 router.delete('/notes/:id', notes.destroy);
+router.get('/health', healthCheck.checkHealth);
 
 module.exports = router;
